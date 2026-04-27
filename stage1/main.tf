25100c93279eb4d6a47444965b272a2721ae472a
HaydenT5 <03071079@nebraska.edu>
2026-04-26 21:54:03 -0500

    Update main.tf

--- stage1/main.tf
+++ stage1/main.tf
@@ -10,8 +10,8 @@ terraform {
   storage_account_name = "acmp2400finalstorageaccount"
   countainer_name = "big-tf-state-acmp2400"
   use_azuread_auth = true
+  }
 }
-
 provider "azurerm" {
   features {}
 }
