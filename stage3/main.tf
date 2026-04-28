b6da9122498e153d58e5dce9793b508120909c55
HaydenT5 <03071079@nebraska.edu>
2026-04-28 13:16:56 -0500

    Create main.tf

---
+++ stage3/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.70.0"
+    }
+  }
+  backend "azurerm" {
+  resource_group_name = "rg-acmp-final"
+  storage_account_name = "acmp2400storageaccount"
+  container_name = "big-tf-state-acmp2400"
+  use_azuread_auth = true
+  }
+}
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_container_registry" "htrotter-acr" {
+  name = "acrhtrotteracmp2400"
+  resource_group_name = "rg-htrotter"
+  location = "Central US"
+  sku = "Basic"
+  admin_enabled = false
+}
